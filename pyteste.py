ade42ec9429211ff56c544270c98a354006023a4
alissonn17 <98761930+alissonn17@users.noreply.github.com>
2022-02-15 23:03:13 +0100

    Criei o arquivo pyteste
    Primeiro arquivo criado no github desktop

---
+++ pyteste.py
@@ -0,0 +1,9 @@
+# Created first archieve in github
+
+x = int(input('Digite um número))
+print('\033[3;33m=-'*20)
+
+if x >= 2:
+    print(f'sim, {x} é maior que 2')
+else:
+    print(f'não, {x} não é maior que 2')
